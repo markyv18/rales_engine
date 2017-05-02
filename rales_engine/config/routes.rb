30456fdab6bfe0117bd90a78116347818c07c342
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2017-05-02 00:24:25 -0600

    complete crud for invoices and items

--- rales_engine/config/routes.rb
+++ rales_engine/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :invoices, except: [:new, :edit]
+      resources :items, except: [:new, :edit]
+      resources :invoice_items, except: [:new, :edit]
+    end
+  end
 end
---
+++ rales_engine/spec/factories/invoices.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :invoice do
+    name "Dr Evil purchase"
+    description "One FaFillion"
+  end
+end
