e9f5b600b61a534388326c1c7525ded8b6e80d3a
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-06-19 23:29:26 -0600

    invoice items relationships and tests passing

--- spec/requests/api/v1/invoice_items/invoice_items_relationships_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_items_relationships_spec.rb
@@ -9,25 +9,25 @@ require 'rails_helper'
 describe "invoice_items relationship APIs" do
   it "returns the invoice for a given invoice_item" do
     invoice = create(:invoice)
-    invoice_item = create(:invoice_item, invoice_id: invoice.id)
+    invoice_item = create(:invoice_item, invoice: invoice)
 
     get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
 
-    invoice = JSON.parse(response.body)
+    invoice = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_success
-    expect(invoice_item['invoice_id']).to eq(invoice['id'])
+    expect(invoice_item[:invoice_id]).to eq(invoice[:id])
   end
 
     it "returns the item for a given invoice_item" do
     item = create(:item)
-    invoice_item = create(:invoice_item, item_id: item.id)
+    invoice_item = create(:invoice_item, item: item)
 
     get "/api/v1/invoice_items/#{invoice_item.id}/item"
 
-    item = JSON.parse(response.body)
+    item = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_success
-    expect(invoice_item['item_id']).to eq(item['id'])
+    expect(invoice_item[:item_id]).to eq(item[:id])
   end
 end
